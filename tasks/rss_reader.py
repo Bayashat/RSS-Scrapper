661bf7fbf04172f4744889d8c5fb9164fee7c678
Bayashat <baiashat2002@gmail.com>
2024-02-18 12:12:37 +0600

    Basic logics for stout is done

--- tasks/rss_reader.py
+++ tasks/rss_reader.py
@@ -1,7 +1,6 @@
-# You shouldn't change  name of function or their arguments
-# but you can change content of the initial functions.
 from argparse import ArgumentParser
 from typing import List, Optional, Sequence
+import xml.etree.ElementTree as ET
 import requests
 
 
@@ -25,17 +24,60 @@ def rss_parser(
     Returns:
         List of strings.
         Which then can be printed to stdout or written to file as a separate lines.
-
-    Examples:
-        >>> xml = '<rss><channel><title>Some RSS Channel</title><link>https://some.rss.com</link><description>Some RSS Channel</description></channel></rss>'
-        >>> rss_parser(xml)
-        ["Feed: Some RSS Channel",
-        "Link: https://some.rss.com"]
-        >>> print("\\n".join(rss_parser(xmls)))
-        Feed: Some RSS Channel
-        Link: https://some.rss.com
     """
-    # Your code goes here
+    try:
+        root = ET.fromstring(xml)
+    except ET.ParseError as e:
+        raise UnhandledException(f"Error parsing XML: {e}")
+
+    output = []
+
+    # Extract information from <channel> element
+    channel = root.find("channel")
+    if channel:
+        title = channel.findtext('title')
+        link = channel.findtext('link')
+        last_build_date = channel.findtext('lastBuildDate')
+        pub_date = channel.findtext('pubDate')
+        language = channel.findtext('language')
+        categories = [category.text for category in channel.findall('.//category')]
+        managing_editor = channel.findtext('managingEditor')
+        description = channel.findtext('description')
+        items = [item.findtext('title') for item in root.findall('.//item')]
+        
+        output.extend([
+            f"Feed: {title}",
+            f"Link: {link}",
+            f"Last Build Date: {last_build_date}",
+            f"Publish Date: {pub_date}",
+            f"Language: {language}",
+            f"Categories: {categories}",
+            f"Editor: {managing_editor}",
+            f"Description: {description}",
+            f"Items: {items}"
+        ])        
+        
+    # Extract information from <item> elements
+    items = root.findall(".//item")
+    for item in items:
+        title = item.findtext('title')
+        author = item.findtext('author')
+        pub_date = item.findtext('pubDate')
+        link = item.findtext('link')
+        category = item.findtext('category')
+        description = item.findtext('description')
+
+        # Format the output for each item
+        output.extend([
+            f"\nTitle: {title}",
+            f"Author: {author}",
+            f"Published: {pub_date}",
+            f"Link: {link}",
+            f"Category: {category}",
+            f"\n{description}"]
+        )
+
+    return output
 
 def main(argv: Optional[Sequence] = None):
     """
